b20f9525ea9b53b70d410d8c85b04e31dee95943
Monika Dziub <dziubmonika@gmail.com>
2020-10-11 21:04:25 +0200

    layout and structure

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const container = document.querySelector('.container')
-let panels = new Array(5).fill(undefined)
+import copy from "./icons/SVG/copy.svg"
+import add from "./icons/SVG/plus.svg"
+import drag from "./icons/SVG/tab.svg"
+import remove from "./icons/SVG/bin2.svg"
+import unlock from "./icons/SVG/unlocked.svg"
+import shades from "./icons/SVG/table2.svg"
+import lock from "./icons/SVG/lock.svg"
+
 
 // html structure for each panel
-const pannelStructureHtml = `
+const panelStructureHtml = `
 <div class="panel__left">
-    <div class="left__add-panel">
-        <img src="icons/SVG/plus.svg" alt="add" />
-    </div>
+<div class="left__add-button">
+<img src=${add} alt="add" />
+</div>
 </div>
 <div class="panel__main">
-    <div class="actions__list">
-        <div class="actions__list-element remove">
-
-        </div>
-        <div class="actions__list-element copy">
-            <img src="icons/SVG/copy.svg" alt="copy" />
-        </div>
-        <div class="actions__list-element drag">
-            <img src="icons/SVG/tab.svg" alt="drag" />
-        </div>
-        <div class="actions__list-element lock">
-            <img src="icons/SVG/unlocked.svg" alt="paddle" />
-        </div>
-        <div class="actions__list-element shades">
-            <img src="icons/SVG/table2.svg" alt="shades" />
-        </div>
-    </div>
-    <div class="colorname"></div>
+<div class="actions__list">
+<div class="actions__list-element remove">
+<img src=${remove} alt="remove"/>
+</div>
+<div class="actions__list-element copy">
+<img src=${copy} alt="copy"/>
+</div>
+<div class="actions__list-element drag">
+<img src=${drag} alt="drag"/>
+</div>
+<div class="actions__list-element paddle unlocked">
+<img src=${unlock} alt="paddle"/>
+</div>
+<div class="actions__list-element shades">
+<img src=${shades} alt="shades"/>
+</div>
+</div>
+<div class="colorname">to be defined</div>
 </div>
 
 <div class="panel__right">
-    <div class="right__add-panel">
-        <img src="icons/SVG/plus.svg" alt="add" />
-    </div>
+<div class="right__add-button">
+<img src=${add} alt="add" />
+</div>
 </div>`
 
-//div creation
+const container = document.querySelector('.container')
+let panels = new Array(5).fill(undefined)
+
 
 //rnadom function
 function randomize (min, max){
@@ -57,13 +65,7 @@ function cratePanels(){
     container.innerHTML = panelsHtml
     const panel = container.querySelectorAll('.panel')
     panel.forEach((p)=>p.style.backgroundColor = `${randomizeColor()}`)
-
-
-
-
-
-
-
+    panel.forEach((p)=>p.innerHTML=`${panelStructureHtml}`)
 }
 
 
