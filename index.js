26340dd780818049712de852dc665dd027b5e4a0
Monika Dziub <dziubmonika@gmail.com>
2020-10-12 23:40:05 +0200

    code refactor

--- index.js
+++ index.js
@@ -7,88 +7,95 @@ import shades from "./icons/SVG/table2.svg";
 import lock from "./icons/SVG/lock.svg";
 
 // html structure for each panel
-const panelStructureHtml = (c, i) => `
-<div class="panel__left">
-<div class="left__add-button">
-<img src=${add} alt="add" data-type="add_left" id="${i}"/>
-</div>
-</div>
-<div class="panel__main">
-<div class="actions__list">
-<div class="actions__list-element remove">
-<img src=${remove} alt="remove" data-type="remove" id="${i}"/>
-</div>
-<div class="actions__list-element copy">
-<img src=${copy} alt="copy" data-type="copy" id="${i}"/>
-</div>
-<div class="actions__list-element drag">
-<img src=${drag} alt="drag" data-type="drag" id="${i}"/>
-</div>
-<div class="actions__list-element paddle unlocked">
-<img src=${unlock} alt="paddle" data-type="paddle" id="${i}"/>
-</div>
-<div class="actions__list-element shades">
-<img src=${shades} alt="shades" data-type="shades" id="${i}"/>
-</div>
-</div>
-<div class="colorname">to be defined</div>
-</div>
 
-<div class="panel__right">
-<div class="right__add-button">
-<img src=${add} alt="add" data-type="add_right" id="${i}"/>
-</div>
+const createPanel = (c, i) => `
+<div class="panel" style="background-color: ${c}" id="panel-${i}">
+    <div class="panel__left">
+        <div class="left__add-button">
+            <img src=${add} alt="add" data-type="add_left" id="${i}"/>
+        </div>
+    </div>
+    <div class="panel__main">
+        <div class="actions__list">
+        <div class="actions__list-element remove">
+            <img src=${remove} alt="remove" data-type="remove" id="${i}"/>
+        </div>
+        <div class="actions__list-element copy">
+            <img src=${copy} alt="copy" data-type="copy" id="${i}"/>
+        </div>
+        <div class="actions__list-element drag">
+            <img src=${drag} alt="drag" data-type="drag" id="${i}"/>
+        </div>
+        <div class="actions__list-element paddle unlocked">
+            <img src=${unlock} alt="paddle" data-type="paddle" id="${i}"/>
+        </div>
+        <div class="actions__list-element shades">
+            <img src=${shades} alt="shades" data-type="shades" id="${i}"/>
+        </div>
+        </div>
+        <div class="colorname">to be defined</div>
+    </div>
+
+    <div class="panel__right">
+        <div class="right__add-button">
+            <img src=${add} alt="add" data-type="add_right" id="${i}"/>
+        </div>
+    </div>
 </div>`;
+
 const container = document.querySelector(".container");
 
-let panels = new Array(5).fill(randomizeColor());
+// {color: 'some color', lock: true | false}
+let panels = new Array(5).fill();
 
 //rnadom function
 function randomize(min, max) {
-  return min + Math.floor((max - min) * Math.random());
+    return min + Math.floor((max - min) * Math.random());
 }
+
 //random color generation
 function randomizeColor() {
-  const hue = randomize(0, 360);
-  const saturation = randomize(0, 100);
-  const lightness = randomize(0, 100);
-  const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-  return color;
+    const hue = randomize(0, 360);
+    const saturation = randomize(0, 100);
+    const lightness = randomize(0, 100);
+    const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+    return color;
+}
+
+function fillArrayWithColors() {
+    const newPanels = panels.map(() => ({ lock: false, color: randomizeColor() }))
+    panels = newPanels
 }
 
 //initial color panels
-function cratePanels() {
-  const panelsHtml = panels
-    .map((el, id) => `<div class="panel" id="panel${id}"></div>`)
-    .join("");
-  container.innerHTML = panelsHtml;
-  const panel = container.querySelectorAll(".panel");
-  panel.forEach((p) => (p.style.backgroundColor = `${randomizeColor()}`));
-  panel.forEach((p, i) => (p.innerHTML = panelStructureHtml(i)));
-  // actionsListUpload()
-  // console.log(removeButton )
-  // return removeButton
+function createPanels() {
+    const panelsHtml = panels.map((panel, id) => createPanel(panel.color, id)).join('')
+    container.innerHTML = panelsHtml;
+    // actionsListUpload()
 }
 
 function cratePalet(e) {
-  if (e.keyCode === 32) {
-    cratePanels();
-  }
+    if (e.keyCode === 32) {
+        fillArrayWithColors()
+        createPanels();
+    }
 }
 
 function removePanel() {
-  console.log("you clicked remove panel button");
+    console.log("you clicked remove panel button");
 }
 
 function actions(e) {
-  console.log(e.path[0].id);
-  if (e.target === container) return;
-  console.log(e.currentTarget);
-  console.log(e.target);
-  const el = e.target;
-  let clickedPanel = this.querySelector(".panel");
-  console.log(clickedPanel);
+    const { dataset, id } = e.path[0];
+
+    switch (dataset.type) {
+        case 'add_left': {
+            panels.splice(id, 0, { lock: false, color: randomizeColor() })
+            createPanels()
+        }
+    }
+
 }
 
-window.addEventListener("keyup", cratePalet, { capture: false });
+window.addEventListener("keyup", cratePalet);
 container.addEventListener("click", actions);
